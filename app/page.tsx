54baf54b23158cb53255caeb7833d9aad3ae7b78
Rhymesdavi <dokeyo390@gmail.com>
2025-07-24 08:53:26 +0300

    Implement complete project set-up

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      Loopus
-    </div>
+    <>
+      <Button>Click Here</Button>
+    </>
   )
 }
 
